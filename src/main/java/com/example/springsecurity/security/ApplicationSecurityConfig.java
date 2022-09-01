e7c42b9409bdafedd2d82c6e5bb980806db0c20f
Sanzhar <taalaibekuulusanzhar@gmail.com>
2022-09-01 22:45:39 +0600

    remember me cookie

--- src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.example.springsecurity.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.example.springsecurity.security.ApplicationUserRole.ADMIN;
 import static com.example.springsecurity.security.ApplicationUserRole.STUDENT;
@@ -39,7 +41,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login").permitAll()
                 .defaultSuccessUrl("/courses",true)
                 .and()
-                .rememberMe(); // defaults to 2 week
+                .rememberMe() // defaults to 2 week
+                .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                .key("somethingverysecured");
     }
 
     @Override
