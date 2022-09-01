fc8c95423feb0fe7f3322e3f5417b25a283e3ce6
Sanzhar <taalaibekuulusanzhar@gmail.com>
2022-09-01 22:22:04 +0600

    custom login page

--- src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
@@ -34,7 +34,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/login").permitAll();
     }
 
     @Override
