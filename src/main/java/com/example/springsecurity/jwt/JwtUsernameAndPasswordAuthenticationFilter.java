8be3a73d86ef728b23b0f1dcf666d5a1cfe3fb3b
Sanzhar <taalaibekuulusanzhar@gmail.com>
2022-09-03 00:06:18 +0600

    successful authentication

--- src/main/java/com/example/springsecurity/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/example/springsecurity/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package com.example.springsecurity.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -8,9 +10,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.security.Key;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class JwtUsernameAndPasswordAuthenticationFilter  extends UsernamePasswordAuthenticationFilter {
     @Autowired
@@ -29,5 +36,22 @@ public class JwtUsernameAndPasswordAuthenticationFilter  extends UsernamePasswor
         }catch (IOException e){
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        String key = "securesecuresecuresecuresecuresecuresecuresecuresecuresecure";
+        String token = Jwts.builder()
+                .setSubject(authResult.getName())
+                .claim("authorities", authResult.getAuthorities())
+                .setIssuedAt(new Date())
+                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusWeeks(2)))
+                .signWith(Keys.hmacShaKeyFor(key.getBytes()))
+                .compact();
+
+        response.addHeader("Authorization","Bearer " + token);
     }
 }
