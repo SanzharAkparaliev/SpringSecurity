c9e403163a09319c1a25ce4c81d146ee9ccd4f57
Sanzhar <taalaibekuulusanzhar@gmail.com>
2022-09-03 22:00:31 +0600

    jwt token verifier filter

--- src/main/java/com/example/springsecurity/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/example/springsecurity/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -3,7 +3,6 @@ package com.example.springsecurity.jwt;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -15,7 +14,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.security.Key;
 import java.time.LocalDate;
 import java.util.Date;
 
